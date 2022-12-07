54f1407182ea2a102fa6debc621f79871154b043
perkles <alef.santos4255@gmail.com>
2022-12-07 18:23:00 -0300

    Adicionado tela de login

--- assets/src/routes.js
+++ assets/src/routes.js
@@ -5,18 +5,21 @@ import { createStackNavigator } from '@react-navigation/stack'
 import Home from './pages/Home'
 import Points from './pages/Points'
 import Detail from './pages/Detail'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
-import Register from './pages/Register/Register'
 const AppStack = createStackNavigator()
 export default function Routes() {
   return (
     <NavigationContainer>
       <AppStack.Navigator
         headerMode="none"
+        initialRouteName="Login"
         screenOptions={{
           cardStyle: { backgroundColor: '#F0F0F5' }
         }}
       >
+        <AppStack.Screen name="Login" component={Login} />
         <AppStack.Screen name="Home" component={Home} />
         <AppStack.Screen name="Points" component={Points} />
         <AppStack.Screen name="Detail" component={Detail} />
